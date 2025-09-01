4c09982f5f441a21ae3b1400d2cb1df5ca1e0f47
Creative-tobi <adeoyeaisha533@gmail.com>
2025-09-01 21:20:37 +0100

    change

--- frontend/src/components/Api.js
+++ frontend/src/components/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://schooldashboard-urtz.onrender.com/db",
+  baseURL: "https://schooldashboard-x2ci.onrender.com/db",
 });
 
 // Attach token if available
