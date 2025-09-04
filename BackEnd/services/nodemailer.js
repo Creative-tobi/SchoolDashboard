f2e36476fbafd79320b43a4b9987644e9ea0bada
Creative-tobi <adeoyeaisha533@gmail.com>
2025-09-04 13:03:46 +0100

    change

--- BackEnd/services/nodemailer.js
+++ BackEnd/services/nodemailer.js
@@ -5,7 +5,7 @@ dotenv.config();
 const Transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE === 'true',
+    secure: process.env.SMTP_SECURE === 'false',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
